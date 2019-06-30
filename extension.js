e66b76e6ba263da2b6ccc0a1843fa2be69f59d3f
svenzaimovic <svenzaimovic@gmail.com>
2019-06-30 14:55:56 +0200

    Rubber duck extension

--- extension.js
+++ extension.js
@@ -1,34 +1,52 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const vscode = require('vscode');
+const path = require('path');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "rubber-duck-vscode-plugin" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('extension.summonDuck', function () {
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		const panel = vscode.window.createWebviewPanel(
+			'rubberDuck', 
+			'Rubber Duck',
+			vscode.ViewColumn.Beside, 
+			{}
+		  );
+
+		  const onDiskPath = vscode.Uri.file(
+			path.join(context.extensionPath, 'media', 'duck.jpeg')
+		  );
+
+		  const duckSrc = onDiskPath.with({ scheme: 'vscode-resource' });
+
+		vscode.window.showInformationMessage('Rubber Duck summoned');
+		panel.webview.html = getWebviewContent(duckSrc);
 	});
 
 	context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
+function getWebviewContent(path) {
+	return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <title>Rubber duck!</title>
+  </head>
+  <body>
+	<h1 style="text-align: center;">Sup?</h1>  
+	<img style="display: block; margin: auto;" src="${path}" width="300" />
+  </body>
+  </html>`;
+  }
+
 function deactivate() {}
 
 module.exports = {
